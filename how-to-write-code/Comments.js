3e5ab893aef08c5252594c5c0e681dd9f7be0263
Djohn-12 <djjohnson1101@gmail.com>
2023-12-19 22:15:36 -0600

    prework included

--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -1,6 +1,6 @@
    // This demo shows off comments!
 
-   // console.log("This does not print.");
+   console.log("This does not print.");
 
    console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
 
@@ -8,5 +8,8 @@
    to have
    multi-line
    comments. */
-
+/* This is my practice comment */ 
+//this makes a comment without needing to close
+/* this creates a comment that goes on until closed
+this continues until closed with */ 
    console.log("Comments make your code more readable by others.");
--- how-to-write-code/consolelogexamples02.js
+++ how-to-write-code/consolelogexamples02.js
@@ -1,3 +1,12 @@
 console.log("Some Programming Languages:");
 
-console.log("Python\nJavaScript\nJava\nC#\nSwift");
+console.log("Python\tJavaScript\tJava\tC#\nSwift");
+console.log( "bruce's beard")
+console.log(Number ('3 3'));
+console.log(Number("three"));
+
+let n = "what's up Doc";
+console.log(n)
+
+console.log(16 - 2 * 5 / 3 + 1)
+console.log(2 ** 2 ** 3 * 3)
