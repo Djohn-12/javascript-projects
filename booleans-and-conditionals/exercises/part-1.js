07d8387db508b3cc7ec658882980dba0c175f4d1
Djohn-12 <djjohnson1101@gmail.com>
2024-01-10 23:42:25 -0600

    assignment done

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -1,6 +1,12 @@
 // Declare and initialize the variables for exercise 1 here:
-
+let engineIndicatorLight = "red blinking";
+let spaceSuitOn = true;
+let shuttleCabinReady = true; 
+let crewStatus = spaceSuitOn && shuttleCabinReady;
+let computerStatusCode = 200;
+let shuttleSpeed = 15000;
 // BEFORE running the code, predict what will be printed to the console by the following statements:
+
 
 if (engineIndicatorLight === "green") {
    console.log("engines have started");
@@ -9,3 +15,38 @@ if (engineIndicatorLight === "green") {
 } else {
    console.log("engines are off");
 }
+
+if (crewStatus === true) {
+   console.log("Crew Ready");
+} else {
+   console.log("Crew Not Ready");
+}
+
+if (computerStatusCode === 200) {
+   console.log("Please stand by. Computer is rebooting.");
+} else if (computerStatusCode === 400) {
+   console.log("Success! Computer online.");
+} else {
+   console.log("Alert: Computer offline!");
+}
+
+if (shuttleSpeed > 17500) {
+   console.log("Alert: Escape velocity reached!");
+} else if (shuttleSpeed < 8000) {
+   console.log("Alert: Cannot maintain orbit!");
+} else {
+   console.log("Stable speed");
+}
+
+if (crewStatus && computerStatusCode === 200 && spaceSuitOn) {
+   console.log("all systems go");
+} else {
+   console.log("WARNING. Not ready");
+}
+
+if (!crewStatus || computerStatusCode !== 200 || !spaceSuitOn) {
+   console.log("WARNING. Not ready");
+} else {
+   console.log("all systems go");
+}
+
