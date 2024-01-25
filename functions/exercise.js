b10c8469fcb021eb799babb57ca97295ce90bd14
Djohn-12 <djjohnson1101@gmail.com>
2024-01-25 16:26:10 -0600

    exercise uploaded

---
+++ functions/exercise.js
@@ -0,0 +1,31 @@
+function makeLine(size) {
+    let line = "";
+    for (let i = 0; i < size; i++) {
+    
+      line += "#";
+    }
+    return line;
+  }
+console.log(makeLine(5));
+
+function makeSquare(size) {
+let square = "";
+    for (let i = 0; i < size; i++) {
+      square += makeLine(size);
+      if (i < size - 1) {
+        square += "\n";
+      }
+    }
+    return square;
+  }
+  
+  console.log(makeSquare(5));
+
+  function makeDownwardStairs(height) {
+    let stairs = '';
+    for (let i = 0; i < height; i++) {
+      stairs += (makeLine(i+1) + '\n');
+    }
+    return stairs.slice(0, -1);
+  }
+  console.log(makeDownwardStairs(5));
