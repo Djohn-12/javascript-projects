e7207e68389b5aa1e5ebf0c801457f8934887b6a
Djohn-12 <djjohnson1101@gmail.com>
2024-01-29 16:53:59 -0600

    raid shuttle

--- more-on-functions/exercises/practice-your-skills.js
+++ more-on-functions/exercises/practice-your-skills.js
@@ -5,11 +5,22 @@ a) If passed a number, return the tripled value.
 b) If passed a string, return the string “ARRR!”
 c) Be sure to test your function before moving on the next part. 
 */
+let createAnon = function(one){
+    if (typeof one === "number"){
+     return one ** 3;
+} else (typeof one === "string")
+     return "ARRR!";
+};
+let arr = ['Elocution', 21, 'Clean teeth', 100];
+console.log(createAnon(42));
+console.log(createAnon("hello"));
 
-/* Add to your code! Use your fuction and the map method to change an array  as follows:
+let newArray = arr.map(createAnon);
+console.log(newArray);
+/* Add to your code! Use your function and the map method to change an array as follows:
 a) Triple any the numbers.
 b) Replace any strings with “ARRR!”
 c) Print the new array to confirm your work.
 */
 
-let arr = ['Elocution', 21, 'Clean teeth', 100];
+
