d2fd79f300b2009d192a1b20f9671dea7a525d73
Djohn-12 <djjohnson1101@gmail.com>
2024-02-09 23:55:37 -0600

    Complete exercise exeptions

--- exceptions/exercises/divide.js
+++ exceptions/exercises/divide.js
@@ -5,3 +5,12 @@
 // However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
 
 // Code your divide function here:
+let divide = (x, y)  => {
+    if (y == 0){
+        throw Error("Attempted to divide by zero.");
+    }
+   return x/y;  
+};
+
+console.log(divide(6,2));
+console.log(divide(3,0));
